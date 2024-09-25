1dfbb5c997e1bc115535aec869d1e1ed559e139f
Theo Jouvencel <tjouvenc@3D3.42angouleme.fr>
2024-09-25 16:51:45 +0200

    moulinette + fix ex04

--- ex04/answer.c
+++ ex04/answer.c
@@ -6,7 +6,7 @@
 /*   By: jcochet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 09:32:53 by jcochet           #+#    #+#             */
-/*   Updated: 2024/09/21 09:40:00 by jcochet          ###   ########.fr       */
+/*   Updated: 2024/09/25 16:51:03 by tjouvenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,21 @@ int	is_space(char c)
 	return (c == ' ' || (c >= '\t' && c <= '\r'));
 }
 
-int count_words(char *str)
+int	count_words(char *str)
 {
 	int	count;
 	int	index;
 
+	index = 0;
 	count = 0;
+	// Si la chaine est vide alors on retourne 0 mots
+	if (str[0] == '\0')
+		return (0);
 	while (str[index])
 	{
-		if (!is_space(str[index]) && (is_space(str[index + 1]) || !str[index + 1]))
+		//On peut mettre une condition sur plusieurs lignes si ca ne rentre pas
+		if (!is_space(str[index]) && (is_space(str[index + 1])
+				|| str[index + 1] == '\0'))
 			count++;
 		index++;
 	}
